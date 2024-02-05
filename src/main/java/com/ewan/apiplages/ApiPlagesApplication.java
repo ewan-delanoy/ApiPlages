5052d20e17ce76d821358a06b7b25df829c01fc6
Ewan Delanoy <ewan.delanoy@zoho.com>
2024-02-05 10:15:48 +0100

    override default Spring Security configuration

--- src/main/java/com/ewan/apiplages/ApiPlagesApplication.java
+++ src/main/java/com/ewan/apiplages/ApiPlagesApplication.java
@@ -1,11 +1,15 @@
 package com.ewan.apiplages;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+// @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class ApiPlagesApplication {
 
 	public static void main(String[] args) {
