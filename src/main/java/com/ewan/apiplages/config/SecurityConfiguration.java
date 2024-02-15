147cc171f2eb618db9900a9731fd23a20d6b1b22
Ewan Delanoy <ewan.delanoy@zoho.com>
2024-02-15 12:54:34 +0100

    use localhost:4200 in SecurityConfiguration

--- src/main/java/com/ewan/apiplages/config/SecurityConfiguration.java
+++ src/main/java/com/ewan/apiplages/config/SecurityConfiguration.java
@@ -53,7 +53,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:8005"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
         configuration.setAllowedMethods(List.of("GET", "POST", "DELETE"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 
