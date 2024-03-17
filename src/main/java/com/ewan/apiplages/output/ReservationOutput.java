54bdfc34f671ea2cfeb7199bb6492d8df0a1da1e
Ewan Delanoy <ewan.delanoy@zoho.com>
2024-03-17 19:25:24 +0100

    add route to API (2)

--- src/main/java/com/ewan/apiplages/output/ReservationOutput.java
+++ src/main/java/com/ewan/apiplages/output/ReservationOutput.java
@@ -3,11 +3,17 @@ package com.ewan.apiplages.output;
 import java.time.LocalDate;
 import java.util.List;
 
-public record ReservationOutput(Long reservationId,
-                                PlageOutput plage, LocalDate dateDebut, LocalDate dateFin,
-                                List<AffectationOutput> affectations,
-                                ClientOutput client,
-                                LienDeParenteOutput lienDeParente, String statutNom,
-                                String numeroCarte, byte moisExpiration, short anneeExpiration,
-                                double totalAvantRemise, double totalApresRemise) {
+public record ReservationOutput(
+        Long reservationId,
+        PlageOutput plage,
+        LocalDate dateDebut,
+        LocalDate dateFin,
+        List<AffectationOutput> affectations,
+        ClientOutput client,
+        LienDeParenteOutput lienDeParente, String statutNom,
+        String numeroCarte,
+        byte moisExpiration,
+        short anneeExpiration,
+        double totalAvantRemise,
+        double totalApresRemise) {
 }
